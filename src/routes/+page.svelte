084727c4d3a77b06311868df4d478adfcccd311a
sohan-fahad <sohanfahad@gmail.com>
2023-05-21 10:48:49 +0600

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div>
+	<h1>svelte with tailwind</h1>
+</div>
+
+<style lang="postcss">
+	div {
+		h1 {
+			@apply text-lg text-red-500;
+		}
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$src: 'src'
+		}
+	},
+	preprocess: vitePreprocess({
+		style: 'postcss'
+	})
+};
+export default config;
